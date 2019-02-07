009719b1c2414e5bf022caffb272d3f692f8b0a8
4803AW16 <4803AW16@usa.miracosta.cc.ca.us>
2019-02-07 13:28:56 -0800

    Initial commit

---
+++ app/src/main/java/edu/miracostacollege/cs134/costalattacars/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.miracostacollege.cs134.costalattacars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import edu.miracostacollege.cs134.costalattacars.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

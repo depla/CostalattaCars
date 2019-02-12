86bc88d783650b18d0657469542e32bbeef5c5b6
Dennis La <dennisla95@yahoo.com>
2019-02-11 19:08:52 -0800

    Update src and xmls

--- app/src/main/java/edu/miracostacollege/cs134/costalattacars/LoanSummaryActivity.java
+++ app/src/main/java/edu/miracostacollege/cs134/costalattacars/LoanSummaryActivity.java
@@ -6,18 +6,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * Activity for the loan summary. Shows the user the data for their car financing.
+ * Allows the user to go back to the data entry activity (MainActivity.java) as well.
+ *
+ * @author Dennis La
+ * @version 1.0
+ */
 public class LoanSummaryActivity extends AppCompatActivity {
 
     private TextView monthlyPaymentTextView;
+    private TextView carStickerPriceTextView;
+    private TextView salesTaxRateTextView;
+    private TextView taxAmountTextView;
+    private TextView downPaymentTextView;
+    private TextView totalCostTextView;
+    private TextView borrowedAmountTextView;
+    private TextView interestAmountTextView;
+    private TextView loanTermTextView;
 
+
+    /**
+     * Grabs the data from MainActivity using an intent. Uses that intent to populate the
+     * textViews
+     * @param savedInstanceState any saved data if applicable
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_summary);
 
         monthlyPaymentTextView = findViewById(R.id.monthlyPaymentTextView);
-        //finish the rest
-
+        carStickerPriceTextView = findViewById(R.id.carStickerPriceTextView);
+        salesTaxRateTextView = findViewById(R.id.salesTaxRateTextView);
+        taxAmountTextView = findViewById(R.id.taxAmountTextView);
+        downPaymentTextView = findViewById(R.id.downPaymentTextView);
+        totalCostTextView = findViewById(R.id.totalCostTextView);
+        borrowedAmountTextView = findViewById(R.id.borrowedAmountTextView);
+        interestAmountTextView = findViewById(R.id.interestAmountTextView);
+        loanTermTextView = findViewById(R.id.loanTermTextView);
 
         //1. receive the intent from main activity
         Intent intent = getIntent();
@@ -25,12 +52,33 @@ public class LoanSummaryActivity extends AppCompatActivity {
 
         //2.populate all the text views
         monthlyPaymentTextView.setText(intent.getStringExtra("MonthlyPayment"));
-        //repeat for other ones
+        carStickerPriceTextView.setText(intent.getStringExtra("CarPrice"));
+        salesTaxRateTextView.setText(intent.getStringExtra("TaxRate"));
+        taxAmountTextView.setText(intent.getStringExtra("TaxAmount"));
+        downPaymentTextView.setText(intent.getStringExtra("DownPayment"));
+        totalCostTextView.setText(intent.getStringExtra("TotalCost"));
+        borrowedAmountTextView.setText(intent.getStringExtra("BorrowedAmount"));
+        interestAmountTextView.setText(intent.getStringExtra("InterestAmount"));
 
-
+        if(intent.getIntExtra("LoanTerm", 0) == 3)
+        {
+            loanTermTextView.setText(getString(R.string.years3));
+        }
+        else if(intent.getIntExtra("LoanTerm", 0) == 4)
+        {
+            loanTermTextView.setText(getString(R.string.years4));
+        }
+        else if(intent.getIntExtra("LoanTerm", 0) == 5)
+        {
+            loanTermTextView.setText(getString(R.string.years5));
+        }
 
     }
 
+    /**
+     * closes this activity. triggered when the "Return to data entry" button is clicked
+     * @param v the button view
+     */
     public void closeActivity(View v)
     {
         this.finish();
--- app/src/main/java/edu/miracostacollege/cs134/costalattacars/MainActivity.java
+++ app/src/main/java/edu/miracostacollege/cs134/costalattacars/MainActivity.java
@@ -14,6 +14,13 @@ import java.util.Locale;
 import edu.miracostacollege.cs134.costalattacars.R;
 import edu.miracostacollege.cs134.costalattacars.model.CarLoan;
 
+/**
+ * Main activity that lets the user enter the car price and down payment and select a loan term.
+ * Lets the user see the report by pressing the "Loan report" button
+ *
+ * @author Dennis La
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
 
 
@@ -26,9 +33,15 @@ public class MainActivity extends AppCompatActivity {
     //lets make a reference to our model (CarLoan)
     private CarLoan loan;
 
-    //lets make formaters for money and percents
+    //lets make formatters for money and percents
     NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.getDefault());
+    NumberFormat percent = NumberFormat.getPercentInstance(Locale.getDefault());
 
+    /**
+     * Instantiates the edit texts and the radio group. also instantiates the model
+     * and makes the percent formatter use decimals to the hundredths place
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,17 +56,43 @@ public class MainActivity extends AppCompatActivity {
         //instantiate a new car loan
         loan = new CarLoan();
 
-
+        //make percents round to the nearest hundredths
+        percent.setMinimumFractionDigits(2);
 
     }
 
+    /**
+     * Extracts the data from the edit text views and the radio button group. updates the model.
+     * Then gets data from the model and then packages the data into an intent to be sent off to
+     * the LoanSummaryActivity. then starts the new activity. triggered by pressing the "Loan
+     * report" button
+     * @param v the button view
+     */
     public void switchToLoanSummary(View v)
     {
         //1) extract data from 2 edit texts and radio group
-        double carPrice = Double.parseDouble(carPriceEditText.getText().toString());
-        double downPayment = Double.parseDouble(downPaymentEditText.getText().toString());
+        double carPrice;
+        double downPayment;
+
+        if(carPriceEditText.getText().toString().length() > 0)
+        {
+            carPrice = Double.parseDouble(carPriceEditText.getText().toString());
+        }
+        else
+        {
+            carPrice = 0;
+        }
+        if(downPaymentEditText.getText().toString().length() > 0)
+        {
+            downPayment = Double.parseDouble(downPaymentEditText.getText().toString());
+        }
+        else
+        {
+            downPayment = 0;
+        }
 
         int loanTerm;
+
         //use a switch statement to determine radio button checked in the radio group
         switch (loanTermRadioGroup.getCheckedRadioButtonId())
         {
@@ -74,8 +113,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-
-
         //2) update the model
         loan.setPrice(carPrice);
         loan.setDownPayment(downPayment);
@@ -87,10 +124,18 @@ public class MainActivity extends AppCompatActivity {
         //share the data
         loanSummaryIntent.putExtra("MonthlyPayment", currency.format(loan.monthlyPayment()));
         loanSummaryIntent.putExtra("CarPrice", currency.format(loan.getPrice()));
-        //need to add the other 7
+        loanSummaryIntent.putExtra("TaxRate", percent.format(loan.OCEANSIDE_TAX_RATE));
+        loanSummaryIntent.putExtra("TaxAmount", currency.format(loan.taxAmount()));
+        loanSummaryIntent.putExtra("DownPayment", currency.format(loan.getDownPayment()));
+        loanSummaryIntent.putExtra("TotalCost", currency.format(loan.totalCost()));
+        loanSummaryIntent.putExtra("BorrowedAmount", currency.format(loan.borrowedAmount()));
+        loanSummaryIntent.putExtra("InterestAmount", currency.format(loan.interestAmount()));
+        loanSummaryIntent.putExtra("LoanTerm", loan.getLoanTerm());
 
 
         //4) start the new activity
         startActivity(loanSummaryIntent);
+
+
     }
 }
